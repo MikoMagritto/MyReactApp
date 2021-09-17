3cd8fde755c5b8e493ae146deaae986f945d8170
MikoMagritto <77416124+MikoMagritto@users.noreply.github.com>
2021-09-17 16:03:30 +0200

    first commit

---
+++ src/temperature.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class temperature extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Temperature</h1>
+                <input type='number'></input>
+            </div>
+        )
+    }
+}
